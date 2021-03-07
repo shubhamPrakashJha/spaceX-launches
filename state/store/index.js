605749f8211564e8251e2d2cdc890a07643a0a04
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2021-03-07 19:28:02 +0530

    [config]: remove devtools

--- state/store/index.js
+++ state/store/index.js
@@ -1,12 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { rootReducer } from '../reducer';
 
 const initialState = {};
 const middleware = [thunkMiddleware];
 
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
 
 export default store;
